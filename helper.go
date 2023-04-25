43cf8b206f437eb540cff2bc8cd672c0e64a3dda
vizee <vizee@live.cn>
2023-04-25 19:32:10 +0800

    feat: using unsafe functions instead of pointer casting

--- helper.go
+++ helper.go
@@ -2,11 +2,6 @@ package jsonpb
 
 import "unsafe"
 
-func noescape(p unsafe.Pointer) unsafe.Pointer {
-	x := uintptr(p)
-	return unsafe.Pointer(x ^ 0)
-}
-
 func asString(s []byte) string {
-	return *(*string)(noescape(unsafe.Pointer(&s)))
+	return unsafe.String(unsafe.SliceData(s), len(s))
 }
